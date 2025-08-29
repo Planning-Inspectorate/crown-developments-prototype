29c5209012d42456bae2cb20400683c124f70750
ameliascanlan-pins <amelia.scanlan.9d@planninginspectorate.gov.uk>
2025-08-29 15:32:26 +0100

    Reorganise page numbers and names

--- app/routes.js
+++ app/routes.js
@@ -13,6 +13,16 @@ const router = govukPrototypeKit.requests.setupRouter();
 
 const SKIP_ANSWERS = false;
 
+router.post('/continue-answer', function (req, res) {
+  if (req.body.continue === 'yes') {
+    res.redirect('/features/application-updates/back-office/6-yes');
+  } else if (req.body.continue === 'no') {
+    res.redirect('/features/application-updates/back-office/5-no');
+  } else {
+    res.redirect('/features/application-updates/back-office/4-publish-update-now');
+  }
+});
+
 router.use(function (req, res, next) {
     if (req.url.endsWith('start')) {
         req.session.data = {}; // clear session on each start
@@ -33,4 +43,3 @@ router.use(function (req, res, next) {
      }
     next();
 });
-
